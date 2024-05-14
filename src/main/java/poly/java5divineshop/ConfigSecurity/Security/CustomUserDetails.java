04d9bed0b877d138f7fa53d307f2d19a8dbabc78
TanLoc060704 <128579533+TanLoc060704@users.noreply.github.com>
2024-05-14 22:12:17 +0700

    JAVA5-03_Commit02_TanLoc: run security loc config

--- src/main/java/poly/java5divineshop/ConfigSecurity/Security/CustomUserDetails.java
+++ src/main/java/poly/java5divineshop/ConfigSecurity/Security/CustomUserDetails.java
@@ -1,51 +1,51 @@
-package poly.java5divineshop.ConfigSecurity.Security;
-
-import lombok.AllArgsConstructor;
-import lombok.NoArgsConstructor;
-import org.springframework.security.core.GrantedAuthority;
-import org.springframework.security.core.userdetails.UserDetails;
-import poly.java5divineshop.Divineshop.Data.Entity.AccountE;
-
-import java.util.Collection;
-@AllArgsConstructor
-@NoArgsConstructor
-public class CustomUserDetails implements UserDetails {
-
-    private AccountE accountE;
-    private Collection<? extends GrantedAuthority> getAuthorities;
-
-    @Override
-    public Collection<? extends GrantedAuthority> getAuthorities() {
-        return getAuthorities;
-    }
-
-    @Override
-    public String getPassword() {
-        return accountE.getHashPassword();
-    }
-
-    @Override
-    public String getUsername() {
-        return accountE.getUsername();
-    }
-
-    @Override
-    public boolean isAccountNonExpired() {
-        return true;
-    }
-
-    @Override
-    public boolean isAccountNonLocked() {
-        return true;
-    }
-
-    @Override
-    public boolean isCredentialsNonExpired() {
-        return true;
-    }
-
-    @Override
-    public boolean isEnabled() {
-        return accountE.isEnabled();
-    }
-}
+//package poly.java5divineshop.ConfigSecurity.Security;
+//
+//import lombok.AllArgsConstructor;
+//import lombok.NoArgsConstructor;
+//import org.springframework.security.core.GrantedAuthority;
+//import org.springframework.security.core.userdetails.UserDetails;
+//import poly.java5divineshop.Divineshop.Data.Entity.AccountE;
+//
+//import java.util.Collection;
+//@AllArgsConstructor
+//@NoArgsConstructor
+//public class CustomUserDetails implements UserDetails {
+//
+//    private AccountE accountE;
+//    private Collection<? extends GrantedAuthority> getAuthorities;
+//
+//    @Override
+//    public Collection<? extends GrantedAuthority> getAuthorities() {
+//        return getAuthorities;
+//    }
+//
+//    @Override
+//    public String getPassword() {
+//        return accountE.getHashPassword();
+//    }
+//
+//    @Override
+//    public String getUsername() {
+//        return accountE.getUsername();
+//    }
+//
+//    @Override
+//    public boolean isAccountNonExpired() {
+//        return true;
+//    }
+//
+//    @Override
+//    public boolean isAccountNonLocked() {
+//        return true;
+//    }
+//
+//    @Override
+//    public boolean isCredentialsNonExpired() {
+//        return true;
+//    }
+//
+//    @Override
+//    public boolean isEnabled() {
+//        return accountE.isEnabled();
+//    }
+//}
--- src/main/java/poly/java5divineshop/ConfigSecurity/Security/SecurityConfig.java
+++ src/main/java/poly/java5divineshop/ConfigSecurity/Security/SecurityConfig.java
@@ -50,53 +50,53 @@ public class SecurityConfig {
 
     // add support for JDBC ... no more hardcoded users :=>
 
-//    @Bean
-//    public UserDetailsManager userDetailsManager (DataSource dataSource){
-//
-//        JdbcUserDetailsManager jdbcUserDetailsManager = new JdbcUserDetailsManager(dataSource);
-//        // define query to retrieve a user by username
-//        jdbcUserDetailsManager.setUsersByUsernameQuery(
-//            "select username,hash_password,is_enabled from account where username = ?"
-//        );
-//        //define query to rerieve the authrities / rolles by username
-//        jdbcUserDetailsManager.setAuthoritiesByUsernameQuery(
-//            "select username,role from roles where username = ?"
-//        );
-//        return jdbcUserDetailsManager;
-//    }
+    @Bean
+    public UserDetailsManager userDetailsManager (DataSource dataSource){
+
+        JdbcUserDetailsManager jdbcUserDetailsManager = new JdbcUserDetailsManager(dataSource);
+        // define query to retrieve a user by username
+        jdbcUserDetailsManager.setUsersByUsernameQuery(
+            "select username,hash_password,is_enabled from account where username = ?"
+        );
+        //define query to rerieve the authrities / rolles by username
+        jdbcUserDetailsManager.setAuthoritiesByUsernameQuery(
+            "select username,role from roles where username = ?"
+        );
+        return jdbcUserDetailsManager;
+    }
 
 
-//    @Bean
-//    public SecurityFilterChain filterChain(HttpSecurity http) throws  Exception{
-//        http
-//                .csrf(csrf -> csrf.disable())
-//                .authorizeHttpRequests(configurer -> configurer
-//                        .requestMatchers("/").permitAll()
-////                        .requestMatchers("/systems/**").hasAuthority("/ADMIN")
-////                        .anyRequest().authenticated()
-//                        .requestMatchers("/cart").authenticated()
-//                        .requestMatchers("/userinfo").hasRole("EMPLOYEE")
-//                        .requestMatchers("/leaders").hasRole("MANAGER")
-//                        .requestMatchers("/systems/**").hasRole("ADMIN")
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws  Exception{
+        http
+                .csrf(csrf -> csrf.disable())
+                .authorizeHttpRequests(configurer -> configurer
+                        .requestMatchers("/").permitAll()
+//                        .requestMatchers("/systems/**").hasAuthority("/ADMIN")
 //                        .anyRequest().authenticated()
-//                )
-//                .formLogin(form ->
-//                        form
-//                                .loginPage("/showMyLoginPage")
-//                                .loginProcessingUrl("/authenticateTheUser")
-//                                .usernameParameter("username")
-//                                .passwordParameter("password")
-////                                .defaultSuccessUrl("/systems",true)
-//                                .permitAll()
-//                )
-//                .logout(logout -> logout.permitAll()
-//                )
-//                .exceptionHandling(configurer ->
-//                        configurer.accessDeniedPage("/access-denied")
-//                );
-//
-//        return http.build();
-//    }
+                        .requestMatchers("/cart").authenticated()
+                        .requestMatchers("/userinfo").hasRole("EMPLOYEE")
+                        .requestMatchers("/leaders").hasRole("MANAGER")
+                        .requestMatchers("/systems/**").hasRole("ADMIN")
+                        .anyRequest().authenticated()
+                )
+                .formLogin(form ->
+                        form
+                                .loginPage("/showMyLoginPage")
+                                .loginProcessingUrl("/authenticateTheUser")
+                                .usernameParameter("username")
+                                .passwordParameter("password")
+//                                .defaultSuccessUrl("/systems",true)
+                                .permitAll()
+                )
+                .logout(logout -> logout.permitAll()
+                )
+                .exceptionHandling(configurer ->
+                        configurer.accessDeniedPage("/access-denied")
+                );
+
+        return http.build();
+    }
 
 //    @Bean
 //    AuthenticationManager authenticationManager(AuthenticationConfiguration authenticationConfiguration)
