7fa2a282e4e07881c7efb13ec393cd7074a1d9d9
PhatOmachi <88134358+PhatOmachi@users.noreply.github.com>
2024-06-10 17:18:32 +0700

    fix config

--- src/main/java/poly/java5divineshop/Divineshop/Api/PaymentAPi.java
+++ src/main/java/poly/java5divineshop/Divineshop/Api/PaymentAPi.java
@@ -17,7 +17,7 @@ import java.io.IOException;
 @RestController
 @RequestMapping("${spring.application.api-prefix}/payment")
 @RequiredArgsConstructor
-public class PaymentAPI {
+public class PaymentAPi {
     private final Paymentservice paymentService;
 
 
